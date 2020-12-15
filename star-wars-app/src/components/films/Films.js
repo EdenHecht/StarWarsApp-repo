49f38c3e08bed1794563293c136ae7ab48c4f05b
Eden Hecht <edenhecht1@gmail.com>
2020-12-15 13:16:49 +0200

    film design (not finished)

--- star-wars-app/src/components/films/Films.js
+++ star-wars-app/src/components/films/Films.js
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import React from "react";
 import { useAllResults } from "../../hooks/hooks";
 import "./films.css";
+import poster from "../../utils/resources/posters/a new hope.jpg";
 
 function Films(props) {
   const [isLoading, info, error] = useAllResults(
@@ -20,10 +21,27 @@ function Films(props) {
           <div className="card-holder">
             {info.map((film, index) => (
               <div className="film-card" key={film.title + "-films"}>
-                <div className="poster-circle"></div>
-                <p>
-                  <Link to={`/film/${film.id}`}>{film.title}</Link>
-                </p>
+                <div className="poster-circle">
+                  <img className="poster-img" src={poster} alt="" />
+                </div>
+                <div className="detail-box">
+                  <div className="left-col">
+                    <Link to={`/film/${film.id}`} className="film-title-link">
+                      {film.title}
+                    </Link>
+                  </div>
+                  <div className="right-col">
+                    <div className="margin-b right-item">
+                      Episode: {film.episode_id}
+                    </div>
+                    <div className="margin-b right-item">
+                      Director: {film.director}
+                    </div>
+                    <div className="right-item">
+                      Release date: {film.release_date}
+                    </div>
+                  </div>
+                </div>
               </div>
             ))}
           </div>
